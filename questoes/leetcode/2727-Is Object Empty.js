d0e086f37af54ba9a427c079305fd4843a903686
Jorge Simões <jorge10_saj@hotmail.com>
2026-03-05 20:06:06 -0300

    add comment to answer question 2727 leetcode

--- questoes/leetcode/2727-Is Object Empty.js
+++ questoes/leetcode/2727-Is Object Empty.js
@@ -2,6 +2,11 @@
  * @param {Object|Array} obj
  * @return {boolean}
  */
+
+/*
+A questão trata a ideia de verificar se é um Array ou um Object e a partir disso, 
+verificar se o Array tem elementos ou se o Object tem chaves.
+*/
 var isEmpty = function (obj) {
   if (Array.isArray(obj)) {
     if (obj.length !== 0) {
